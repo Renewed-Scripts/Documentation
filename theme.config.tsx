88c8fe158e5433a158677a343a64653ffffbcb87
FjamZoo <laurids112@live.dk>
2024-04-30 07:24:17 +0200

    fix: redo theme settings

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,23 +1,13 @@
-import React from 'react'
-import { DocsThemeConfig, useConfig } from 'nextra-theme-docs';
-import { useRouter } from 'next/router';
+import React from "react";
+import { DocsThemeConfig, useConfig } from "nextra-theme-docs";
+import { useRouter } from "next/router";
 
-// function useHead() {
-//   return (
-//     <>
-//       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-//       <meta property="og:title" content="Renewed Scripts" />
-//       <meta property="og:description" content="Documentation for Renewed Scripts" />
-//     </>
-//   );
-// }
-
-
+// https://github.com/overextended/overextended.github.io/blob/main/theme.config.tsx#L6-L37
 function useHead() {
   const { asPath } = useRouter();
   const { frontMatter, title } = useConfig();
   const url = `https://renewed.dev${asPath}`;
-  const description = frontMatter.description || "Documentation for Renewed resources for FiveM";
+  const description = frontMatter.description || "Documentation for the Renewed Scripts";
 
   return (
     <>
@@ -34,76 +24,48 @@ function useHead() {
 
 function useNextSeoProps() {
   const { asPath } = useRouter();
-  const arr = asPath.replace(/[-_]/g, ' ').split('/');
-  const category = (arr[1][0] !== '#' && arr[1]) || 'Docs';
+  const arr = asPath.replace(/[-_]/g, " ").split("/");
+  const category = (arr[1][0] !== "#" && arr[1]) || "Renewed Scripts";
   const rawTitle = arr[arr.length - 1];
-  const title = /[a-z]/.test(rawTitle) && /[A-Z]/.test(rawTitle) ? rawTitle : '%s';
+  const title = /[a-z]/.test(rawTitle) && /[A-Z]/.test(rawTitle) ? rawTitle : "%s";
 
   return {
     titleTemplate: `${title} - ${
-      rawTitle === category ? 'Documentation' : category.replace(/(^\w|\s\w)/g, (m) => m.toUpperCase())
+      rawTitle === category ? "Documentation" : category.replace(/(^\w|\s\w)/g, (m) => m.toUpperCase())
     }`,
   };
 }
-
-
-
-
-// const config: DocsThemeConfig = {
-//   logo: <span>Renewed Scripts</span>,
-//   project: {
-//     link: 'https://github.com/Renewed-Scripts',
-//   },
-//   chat: {
-//     link: 'https://discord.com/invite/5M2ajFnQeh',
-//   },
-//   docsRepositoryBase: 'https://github.com/Renewed-Scripts/Documentation',
-//   footer: {
-//     text: 'Renewed Scripts',
-//   },
-
-//   sidebar: {
-//     defaultMenuCollapseLevel: 1,
-//   },
-
-//   head: useHead,
-// }
-
 
 const config: DocsThemeConfig = {
   logo: (
     <div
       style={{
-        paddingLeft: '50px',
-        lineHeight: '40px',
-        background: "url('https://avatars.githubusercontent.com/u/103452336?s=200&v=4') no-repeat left",
-        backgroundSize: '40px',
+        paddingLeft: "50px",
+        lineHeight: "40px",
+        background: "url(https://avatars.githubusercontent.com/u/103452336?s=200&v=4) no-repeat left",
+        backgroundSize: "38px",
         fontWeight: 550,
       }}
     >
-      Documentation
+      Renewed Scripts
     </div>
-  ),  project: {
-    link: 'https://github.com/Renewed-Scripts',
+  ),
+  project: {
+    link: "https://github.com/Renewed-Scripts",
   },
   chat: {
-    link: 'https://discord.com/invite/5M2ajFnQeh',
+    link: "https://discord.com/invite/5M2ajFnQeh",
   },
-  docsRepositoryBase: 'https://github.com/Renewed-Scripts/Documentation',
+  docsRepositoryBase: "https://github.com/Renewed-Scripts/Documentation",
   footer: {
-    text: 'Renewed Scripts',
+    text: "Renewed Scripts",
   },
-  head: useHead,
   primaryHue: { dark: 200, light: 200 },
   sidebar: {
     defaultMenuCollapseLevel: 1,
   },
+  head: useHead,
   useNextSeoProps: useNextSeoProps,
-}
+};
 
-
-
-
-
-
-export default config
+export default config;
